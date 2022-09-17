f090bbf9cd57c56a88f2ec82153d92a6b6491047
hungleung <hungleung.lee@me.com>
2022-09-17 09:13:42 +0800

    Update LEDE-Configurator-32M.sh
    adjust basic package
    remove IPV6 and WLAN

--- diy-part2/LEDE-Configurator-32M.sh
+++ diy-part2/LEDE-Configurator-32M.sh
@@ -28,10 +28,10 @@ modification() {
 
 add_packages(){
     [ -e is_add_packages ] && echo Add packages is done already. && return 0
-    
+
     # 修改一些依赖
     modification
-    
+
     # 已修改标志（其实也就DEBUG的时候有用）
     touch is_add_packages
 }
@@ -42,6 +42,12 @@ config_clean() {
     #=========================================
     cat >> .config << EOF
 CONFIG_STRIP_KERNEL_EXPORTS=y
+CONFIG_PACKAGE_e2fsprogs=y
+CONFIG_PACKAGE_fdisk=y
+CONFIG_PACKAGE_luci-app-ssr-plus_INCLUDE_SagerNet_Core=y
+CONFIG_PACKAGE_luci-app-ssr-plus_INCLUDE_Shadowsocks_NONE_Client=y
+CONFIG_PACKAGE_luci-app-udpxy=y
+CONFIG_PACKAGE_luci-app-wireguard=y
 # CONFIG_USE_MKLIBS is not set
 EOF
     #=========================================
@@ -49,15 +55,78 @@ EOF
     #=========================================
     cat >> .config << EOF
 # ----------luci-app-ssr-plus
-# CONFIG_PACKAGE_luci-app-ssr-plus is not set
+# CONFIG_PACKAGE_ddns-scripts_aliyun is not set
+# CONFIG_PACKAGE_ddns-scripts_dnspod is not set
+# CONFIG_PACKAGE_luci-app-ddns is not set
+# CONFIG_PACKAGE_luci-app-autoreboot is not set
+# CONFIG_PACKAGE_luci-app-arpbind is not set
+# CONFIG_PACKAGE_luci-app-filetransfer is not set
+# CONFIG_PACKAGE_luci-app-vsftpd is not set
+# CONFIG_PACKAGE_luci-app-accesscontrol is not set
+# CONFIG_PACKAGE_luci-app-nlbwmon is not set
+# CONFIG_PACKAGE_luci-app-wol is not set
+# CONFIG_PACKAGE_luci-app-vsftpd is not set
+
 
 EOF
     #=========================================
     # unset some default to avoid duplication
+    # unset IPV6 and WLAN
     #=========================================
     cat >> .config << EOF
-# CONFIG_PACKAGE_luci-app-passwall_Transparent_Proxy is not set
-# CONFIG_PACKAGE_luci-app-passwall2_Transparent_Proxy is not set
+# CONFIG_BUSYBOX_DEFAULT_FEATURE_IPV6 is not set
+# CONFIG_DRIVER_11AC_SUPPORT is not set
+# CONFIG_DRIVER_11N_SUPPORT is not set
+# CONFIG_IPV6 is not set
+# CONFIG_KERNEL_IPV6 is not set
+# CONFIG_OPENLDAP_DEBUG is not set
+# CONFIG_PACKAGE_hostapd-common is not set
+# CONFIG_PACKAGE_iw is not set
+# CONFIG_PACKAGE_iwinfo is not set
+# CONFIG_PACKAGE_kmod-cfg80211 is not set
+# CONFIG_PACKAGE_kmod-crypto-arc4 is not set
+# CONFIG_PACKAGE_kmod-crypto-ccm is not set
+# CONFIG_PACKAGE_kmod-crypto-cmac is not set
+# CONFIG_PACKAGE_kmod-crypto-ctr is not set
+# CONFIG_PACKAGE_kmod-crypto-ecb is not set
+# CONFIG_PACKAGE_kmod-crypto-gcm is not set
+# CONFIG_PACKAGE_kmod-crypto-gf128 is not set
+# CONFIG_PACKAGE_kmod-crypto-ghash is not set
+# CONFIG_PACKAGE_kmod-crypto-hmac is not set
+# CONFIG_PACKAGE_kmod-crypto-rng is not set
+# CONFIG_PACKAGE_kmod-crypto-seqiv is not set
+# CONFIG_PACKAGE_kmod-crypto-sha1 is not set
+# CONFIG_PACKAGE_kmod-crypto-sha256 is not set
+# CONFIG_PACKAGE_kmod-hwmon-core is not set
+# CONFIG_PACKAGE_kmod-lib-crc-ccitt is not set
+# CONFIG_PACKAGE_kmod-mac80211 is not set
+# CONFIG_PACKAGE_kmod-macvlan is not set
+# CONFIG_PACKAGE_kmod-mt7603 is not set
+# CONFIG_PACKAGE_kmod-mt7615-firmware is not set
+# CONFIG_PACKAGE_kmod-mt7615e is not set
+# CONFIG_PACKAGE_libip6tc is not set
+# CONFIG_PACKAGE_luci-app-diskman_INCLUDE_btrfs_progs is not set
+# CONFIG_PACKAGE_luci-app-diskman_INCLUDE_lsblk is not set
+# CONFIG_PACKAGE_luci-app-passwall_INCLUDE_ChinaDNS_NG is not set
+# CONFIG_PACKAGE_luci-app-passwall_INCLUDE_ShadowsocksR_Libev_Client is not set
+# CONFIG_PACKAGE_luci-app-passwall_INCLUDE_Shadowsocks_Libev_Client is not set
+# CONFIG_PACKAGE_luci-app-passwall_INCLUDE_Simple_Obfs is not set
+# CONFIG_PACKAGE_luci-app-passwall_INCLUDE_Trojan_Plus is not set
+# CONFIG_PACKAGE_luci-app-rclone_INCLUDE_rclone-ng is not set
+# CONFIG_PACKAGE_luci-app-rclone_INCLUDE_rclone-webui is not set
+# CONFIG_PACKAGE_luci-app-ssr-plus_INCLUDE_IPT2Socks is not set
+# CONFIG_PACKAGE_luci-app-ssr-plus_INCLUDE_NONE_V2RAY is not set
+# CONFIG_PACKAGE_luci-app-ssr-plus_INCLUDE_ShadowsocksR_Libev_Client is not set
+# CONFIG_PACKAGE_luci-app-ssr-plus_INCLUDE_Shadowsocks_Libev_Client is not set
+# CONFIG_PACKAGE_luci-app-ssr-plus_INCLUDE_Shadowsocks_Simple_Obfs is not set
+# CONFIG_PACKAGE_luci-app-ssr-plus_INCLUDE_Trojan is not set
+# CONFIG_PACKAGE_shellsync is not set
+# CONFIG_PACKAGE_wireless-regdb is not set
+# CONFIG_PACKAGE_wpad-basic-openssl is not set
+# CONFIG_PCAP_DNSPROXY_LIBPCAP is not set
+# CONFIG_PCAP_DNSPROXY_LIBSODIUM is not set
+# CONFIG_PCAP_DNSPROXY_TLS is not set
+# CONFIG_TARGET_ROOTFS_INITRAMFS is not set
 EOF
 }
 
